308537565433a204fac6d288e34314c6f040b9ef
dataisland98 <dataisland@outlook.com>
2018-11-30 22:24:00 +0800

    ok

---
+++ rem/ext.hpp
@@ -0,0 +1,63 @@
+#ifndef EXT_HPP
+#define EXT_HPP
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+#include <iostream>
+
+namespace sjtu{
+    using string = std::string;
+    using parameter = std::vector<string>;
+
+    void format_print(int x) {
+        std::cout << x / 100 << '.';
+        x %= 100;
+        if (x == 0) std::cout << "00";
+        else if (x <= 9) std::cout << "0" << x;
+        else std::cout << x;
+    }
+    
+    int to_int(const string& str) {
+        std::istringstream os(str);
+   	double value; os >> value;
+        return value;
+    }
+
+    int to_int_100(const string& str) {
+        std::istringstream os(str);
+   	double value; os >> value;
+        //fprintf(stderr, "%.3f %d\n", value, int(value * 100)); 
+        return round(value * 100);
+    }
+    
+    parameter split(const string& str, char ch = ' ') {
+        parameter retval;
+        size_t i, last; bool flag = true;
+        for (i = 0, last = 0; i < str.size(); ++i) {
+            if (str[i] == '"') flag ^= 1;
+            if (str[i] == ch && flag) {
+                if (i - last > 0)
+                    retval.push_back(str.substr(last, i - last));
+                last = i + 1;
+            }
+        }
+        if (i - last > 0)
+            retval.push_back(str.substr(last, i - last));
+        return retval;
+    }
+
+    bool format(const string& str, string& attr, string& content) {
+        if (str.front() != '-') return false;
+        for (size_t i = 5; i < str.size(); ++i)
+            if (str[i] == '=') {
+                attr = str.substr(1, i - 1);
+                content = str.substr(i + 1, str.size() - i - 1);
+                return true;
+            }
+        return false;
+    }
+}
+
+#endif
---
+++ rem/finance.hpp
@@ -0,0 +1,109 @@
+#ifndef FINANCE_HPP
+#define FINANCE_HPP
+
+#include <algorithm>
+namespace sjtu{
+    using i64 = long long;
+    void print(i64 x, FILE *fp) {
+        for (int i = 0; i < 64; i += 8) 
+            fputc(x >> i & 255, fp);
+    }
+
+    i64 input(FILE* fp) {
+        i64 x = 0;
+        for (int i = 0; i < 64; i += 8) 
+            x |= (i64) fgetc(fp) << i;
+        return x;
+    }
+
+    void create(const std::string file) {
+        FILE *f = fopen(file.c_str(), "w");
+        fclose(f);
+    }
+    
+    class costSystem{
+        static const int BLOCKSIZE = 2048;
+
+        FILE *base;
+        FILE *data;
+
+        int p;
+        i64 size;
+        std::vector<i64> sum;
+        
+        int get_id(int size) {
+            return size / BLOCKSIZE;
+        }
+        
+        i64 get(int pos) {
+            fseek(data, pos * 8, SEEK_SET);
+            return input(data);
+        }
+
+        void write(int pos, i64 val) {
+            fseek(data, pos * 8, SEEK_SET);
+            print(val, data);
+        }
+
+        i64 calc(int id, int l, int r) {
+            int lb = id * BLOCKSIZE, rb = lb + BLOCKSIZE - 1;
+            if (l <= lb && rb <= r) return sum[id];
+            else {
+                i64 ret = 0;
+                for (int i = std::max(l, lb); i <= std::min(r, rb); ++i)
+                    ret += get(i);
+                return ret;
+            }
+        }
+        
+    public:
+        costSystem () = default;
+        void init (const std::string file) {
+            base = fopen((file + ".base.bin").c_str(), "r+");
+            data = fopen((file + ".data.bin").c_str(), "r+");
+
+            if (base == nullptr) {
+                create(file + ".base.bin");
+                create(file + ".data.bin");
+                base = fopen((file + ".base.bin").c_str(), "r+");
+                data = fopen((file + ".data.bin").c_str(), "r+");
+                print(0LL, base);
+                print(0LL, base);
+            }
+
+            rewind(base);
+            size = input(base);
+            sum.resize(size / BLOCKSIZE + 1);
+            for (auto &v : sum)
+                v = input(base);
+        }
+        virtual ~costSystem () {
+            rewind(base);
+            print(size, base);
+            for (auto v : sum)
+                print(v, base);
+            fclose(base);
+            fclose(data);
+        }
+
+        void append(i64 x) {
+            write(++size, x);
+            sum.back() += x;
+            if (size % BLOCKSIZE == 0)
+                sum.emplace_back(0);
+        }
+
+        i64 find(int time = -1) {
+            if (time == -1) return std::accumulate(sum.begin(), sum.end(), 0LL);
+            else {
+                i64 ret = 0;
+                int l = size - time + 1, r = size;
+                for (int i = get_id(l); i <= get_id(r); ++i)
+                    ret += calc(i, l, r);
+                return ret;
+            }
+        }
+    };
+}
+
+#endif
